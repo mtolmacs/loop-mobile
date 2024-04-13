b0d58ba3440cb2c0193ff14cf7b6041cfd49affb
Mark Tolmacs <mark@lazycat.hu>
2024-04-13 21:06:04 +0200

    Property detail rendering
    Signed-off-by: Mark Tolmacs <mark@lazycat.hu>

--- app/[property].tsx
+++ app/[property].tsx
@@ -1,9 +1,10 @@
 import { loadDetailsAtom } from '#/properties'
+import { Gallery } from '@/Gallery'
 import Loading from '@/Loading'
-import { Redirect, useLocalSearchParams } from 'expo-router'
+import { Redirect, Stack, useLocalSearchParams } from 'expo-router'
 import { useAtom } from 'jotai'
 import React from 'react'
-import { Paragraph } from 'tamagui'
+import { H3, Paragraph, XStack, YStack } from 'tamagui'
 
 export default function PropertyDetailsPage() {
   const { property: propertyId } = useLocalSearchParams()
@@ -25,5 +26,19 @@ export default function PropertyDetailsPage() {
     return <Redirect href="/error" />
   }
 
-  return <Paragraph m="$4">{property && JSON.stringify(property)}</Paragraph>
+  const images = property.images?.map(
+    (image) => new URL(image.url?.length > 0 ? image.url : 'https://placehold.co/300x200')
+  )
+
+  return (
+    <XStack flex={1}>
+      <YStack flex={1} m="$4" alignItems="center" gap="$4">
+        <YStack gap="$4" maxWidth={820}>
+          <H3>{property.propertyTypeText} on {property.address}</H3>
+          <Gallery images={images} />
+        </YStack>
+      </YStack>
+      <Stack.Screen options={{ title: property.propertyTypeText }} />
+    </XStack>
+  )
 }
